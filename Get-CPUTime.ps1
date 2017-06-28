27090cdcae0154e4c81b5eda457384465971e09a
brunoscota <brunoscota@gmail.com>
2017-06-28 01:29:38 -0300

    First Commit

---
+++ Get-CPUTime.ps1
@@ -0,0 +1,45 @@
+﻿param(
+    [int]$warn,
+    [int]$crit
+)
+
+$countline = ""
+$perfline = ""
+$outline = ""
+#$warn = 4
+#$crit = 50
+$warnsignal = 0
+$critsignal = 0
+$counter = (Get-Counter "\Processor(*)\% Processor time").CounterSamples | Select-Object InstanceName,CookedValue
+
+
+foreach ($i in $counter){
+    $floated = ([math]::round($i.CookedValue,2))
+
+    if(($floated -ge $warn) -and ($floated -lt $crit)){
+        $warnsignal++
+        #$i.InstanceName+" "+$floated+"WARNING"
+    }
+        elseif(($floated -gt $warn) -and ($floated -gt $crit)){
+            $critsignal++
+            #$i.InstanceName+" "+$floated+"CRITICAL"
+        }
+
+    $countline += ("CPU "+[string]$i.InstanceName+": "+$floated+"% ")
+    $perfline += ("CPU"+[string]$i.InstanceName+"="+$floated+"%;"+$warn+"%;"+$crit+"%; ")
+}
+
+$outline = $countline+"| "+$perfline
+
+if ($critsignal -gt 0){
+    Write-Host "$critsignal CPU(s) in CRITICAL state - $outline"
+	exit(2)
+}
+    elseif ($warnsignal -gt 0){
+        Write-Host "$warnsignal CPU(s) in WARNING state - $outline"
+		exit(1)
+        }
+        else{
+            Write-Host "CPU(s) OK - $outline"
+			exit(0)
+            }
